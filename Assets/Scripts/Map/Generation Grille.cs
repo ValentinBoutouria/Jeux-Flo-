cb70f3cb8cd4c6b50820f0a2b42b4e4faa112157
ValentinBoutouria <v.boutouria159@gmail.com>
2024-04-03 10:43:21 +0100

    Centrage de la cam en fonction du nb de cellule

--- Assets/Scripts/Map/Generation Grille.cs
+++ Assets/Scripts/Map/Generation Grille.cs
@@ -6,12 +6,15 @@ public class GenerationGrille : MonoBehaviour
 {
     // Start is called before the first frame update
   
-    public GameObject cellPrefab; // Préfabriqué de la cellule de la grille
+    public GameObject cellPrefab; // Prï¿½fabriquï¿½ de la cellule de la grille
+    public GameObject cam; // GameObject de la camera
     public int gridSizeX = 5; // Nombre de cellules en largeur
     public int gridSizeY = 5; // Nombre de cellules en hauteur
     public float gridEcartX = 1f; // Nombre D'ecart entre les cellules en X
     public float gridEcartY = 1f; // Nombre D'ecart entre les cellules en Y
     public float cellSize = 1f; // Taille de chaque cellule
+    private float posCamX; //Pos camX
+    private float posCamY; //Pos camY
 
     void Start()
     {
@@ -20,7 +23,8 @@ public class GenerationGrille : MonoBehaviour
 
     void GenerateGrid()
     {
-        // Boucle pour créer chaque cellule de la grille
+        CalibrageCamera();
+        // Boucle pour crï¿½er chaque cellule de la grille
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
@@ -28,11 +32,20 @@ public class GenerationGrille : MonoBehaviour
                 // Calcule la position de la cellule dans la grille
                 Vector3 cellPosition = new Vector3(x * (cellSize+gridEcartX), 0, y * (cellSize+ gridEcartY));
 
-                // Instancie une nouvelle cellule de la grille à la position calculée
+                // Instancie une nouvelle cellule de la grille ï¿½ la position calculï¿½e
                 GameObject newCell = Instantiate(cellPrefab, cellPosition, Quaternion.identity);
-                newCell.transform.localScale = new Vector3(cellSize, 1f, cellSize); // Réglage de la taille de la cellule
-                newCell.transform.parent = transform; // Définit le parent de la cellule en tant que gameObject de grille pour une organisation propre
+                newCell.transform.localScale = new Vector3(cellSize, 1f, cellSize); // Rï¿½glage de la taille de la cellule
+                newCell.transform.parent = transform; // Dï¿½finit le parent de la cellule en tant que gameObject de grille pour une organisation propre
             }
         }
+    }
+    void CalibrageCamera()
+    {
+        posCamX=gridSizeX/2;
+        posCamY=gridSizeY/2;
+        Vector3 posCamVect=new Vector3 (posCamX,15.0f,posCamY);
+        cam.transform.localPosition =posCamVect;
+
+
     }
 }
