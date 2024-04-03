dd7f71fa91c2db497b8b2bb9a516f560d4730429
ValentinBoutouria <v.boutouria159@gmail.com>
2024-04-03 11:26:32 +0100

    Selection + Deselection

--- Assets/Scripts/Cubes/Deselction.cs
+++ Assets/Scripts/Cubes/Deselction.cs
@@ -2,45 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Selection : MonoBehaviour
+public class Deselction : MonoBehaviour
 {
-    public Material _matSelection; // Le nouveau mat�riau que vous souhaitez appliquer
-    private Material _matInitial; //Material initial de l'hexagone
-    private Deselction deselction;
-
+    public bool Select=false;
+    public Material _matInitial;
     // Start is called before the first frame update
     void Start()
     {
+        //Renderer renderer = GetComponent<Renderer>();
+        //_matInitial=renderer.material;
         
     }
 
     // Update is called once per frame
     void Update()
     {
- 
-        
-        // V�rifie si l'utilisateur clique avec le bouton gauche de la souris
-        if (Input.GetMouseButtonDown(0))
+        // V�rifie si l'utilisateur clique avec le bouton Droit de la souris
+        if (Input.GetMouseButtonDown(1))
         {
             // Convertit la position du curseur de la souris en un rayon dans la sc�ne
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            
 
             // Lance un rayon et v�rifie s'il a touch� un objet avec le tag "Cube"
             if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Cube"))
             {
                 // L'objet touch� a le tag "Cube", vous pouvez impl�menter ici la logique de s�lection
                 //Debug.Log("Cube s�lectionn� : " + hit.collider.gameObject.name);
-                deselction=hit.collider.gameObject.GetComponent<Deselction>();//on recupere le script deselection pour changer le bool
-                Renderer renderer = hit.collider.GetComponent<Renderer>();//on recupere le renderer
-                renderer.material = _matSelection;//on place le mat selection
-                deselction.Select=true;//on met le bool Selection a true
-                
+                Renderer renderer = hit.collider.GetComponent<Renderer>();
+                Deselction deselect =hit.collider.GetComponent<Deselction>();
+                if(renderer.material !=_matInitial)
+                {
+                    deselect.Select=false;
+                    renderer.material= _matInitial;
+                }
             }
         }
-
+        
     }
-   
-    
 }
