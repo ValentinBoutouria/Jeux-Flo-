d6585a054b9a324195ab7a1e4c859ed3a9dd8130
ValentinBoutouria <v.boutouria159@gmail.com>
2024-04-04 16:33:21 +0100

    ajout pierre et gold point interafctif

--- Assets/Scripts/Interaction/Ressources/Stone.cs
+++ Assets/Scripts/Interaction/Ressources/Stone.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Stone : MonoBehaviour
 {
+    public int _nbStone;
+    public TMP_Text tMP_Text;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Stone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        tMP_Text.text="Stone : "+_nbStone;
     }
 }
