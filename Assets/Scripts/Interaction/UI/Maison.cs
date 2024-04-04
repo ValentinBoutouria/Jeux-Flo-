37e241ddf4337267284430b7e473bbfec5b079f4
ValentinBoutouria <151946292+ValentinBoutouria@users.noreply.github.com>
2024-04-04 18:01:02 +0100

    Update Maison.cs

--- Assets/Scripts/Interaction/UI/Maison.cs
+++ Assets/Scripts/Interaction/UI/Maison.cs
@@ -8,12 +8,8 @@ public class Maison : MonoBehaviour
     public int _prix;
     public int _nbMaisonsAchete;
     public GameObject _validationUI;
-<<<<<<< HEAD
-    public GameObject InstantiateMaison; // Référence vers le prefab de l'objet à instancier
-=======
     public Selection _selection;
     public GameObject _maisonPrefabLVL1;
->>>>>>> d6585a054b9a324195ab7a1e4c859ed3a9dd8130
 
     // Start is called before the first frame update
     void Start()
@@ -31,13 +27,6 @@ public class Maison : MonoBehaviour
     {
         _nbMaisonsAchete=_selection.nbGameObjectSelect;
         _validationUI.SetActive(true);
-<<<<<<< HEAD
-        InstantiateMaison.gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);//changement du scale de la maison
-        GameObject Maison = Instantiate(InstantiateMaison, Vector3.zero + new Vector3(0f, 0.2f,0f), Quaternion.identity) ;//a changer
-
-
-
-=======
         foreach (GameObject obj in _selection.gameObjectListSelectionne)//pour tout les gameobject dans liste selectionne
         {
           GameObject GameObjectTemp=Instantiate(_maisonPrefabLVL1,obj.transform.position+new Vector3(0,0.3f,0),Quaternion.Euler(0, -90, 0),obj.transform);
@@ -47,6 +36,5 @@ public class Maison : MonoBehaviour
           _selection.listeGameObjectNONSelect.Add(obj);
         }
         _selection.gameObjectListSelectionne.Clear();
->>>>>>> d6585a054b9a324195ab7a1e4c859ed3a9dd8130
     }
 }
